4fbe854ad6b0871282b053aa292a9a9ecf4f58be
clarkmaio <maioliandrea0@gmail.com>
2020-08-08 19:49:16 +0200

    Loss curve plot + SHAP plots

--- clark_BDT.py
+++ clark_BDT.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import numpy as np
 import pickle
+import os
 from pprint import pprint
+import sys
 
 import shap
 from xgboost import XGBRegressor
@@ -9,6 +11,7 @@ from sklearn.model_selection import RandomizedSearchCV, GridSearchCV
 
 from plotly.offline import plot
 from plotly import graph_objs as go
+import matplotlib.pyplot as plt
 
 class clark_BDT(object):
     """
@@ -18,39 +21,55 @@ class clark_BDT(object):
         - grid_search: dict with keys:
             - domain
             - params
+
+            - mdl_path: path where model will be saved
     """
     def __init__(self, mdl_config):
         self.mdl_config = mdl_config
-        self.mdl_constructor()
+        self._initialize_mdl()
 
-    def mdl_constructor(self):
+    def _initialize_mdl(self):
+        """
+        Initilize model.
+        Create mdl_path
+        """
+        if not os.path.exists(self.mdl_config['mdl_path']):
+            os.mkdir(self.mdl_config['mdl_path'])
         self.mdl = XGBRegressor(**self.mdl_config['params'])
 
-    def fit(self, X, y):
-        self.mdl.fit(X, y)
+    def fit(self, X, y, **kwargs):
+        print('>>>>> Fitting BDT mdl...')
+        self.mdl.fit(X, y, **kwargs)
 
-    def predict(self, X):
-        y = self.mdl.predict(X)
+    def predict(self, X, **kwargs):
+        y = self.mdl.predict(X, **kwargs)
 
         if isinstance(X, pd.DataFrame):
             y = pd.DataFrame(y, index = X.index, columns = ['pred'])
 
         return y
 
-    def save(self, path):
+    def save(self):
         """
         Save mdl object.
         path is the full path: /.../mdl.p
         """
-        return 0
+        print('>>>>> Saving BDT mdl...')
+        mdl_path = os.path.join(self.mdl_config['mdl_path'], 'mdl_BDT.p')
+        pickle.dump(self.mdl, open(mdl_path, 'wb'))
 
-    def load(self, path):
+
+    def load(self):
         """
         Save mdl object.
         path is the full path: /.../mdl.p
         """
-        return 0
+        print('>>>>> Loading BDT mdl...')
+        mdl_path = os.path.join(self.mdl_config['mdl_path'], 'mdl_BDT.p')
+        self.mdl = pickle.load(open(mdl_path, 'rb'))
 
+    # ---------------------- GRID SEARCH ---------------------------
+    # TODO option for Random grid search
     def grid_search(self, X, y):
 
         print('>>>>> Starting grid search...')
@@ -63,19 +82,60 @@ class clark_BDT(object):
 
         return gs.best_params_
 
-    def shap(self, X, y):
-        self.explainer = shap.TreeExplainer(self.mdl)
-        self.shap_values = self.explainer.shap_values(X)
+    # ---------------------- SHAP ---------------------------
+    def _initialize_shap(self, *args, **kwargs):
+        """
+        Inizialize shap object once you have fitted mdl
+        """
+        print('>>>>> Initialize SHAP mdl...')
+        self.shap_explainer = shap.TreeExplainer(self.mdl)
+        self.shap_values = self.shap_explainer.shap_values(*args, **kwargs)
 
-    def force_plot(self, X):
-        # TODO: Take as input X and sycronise to sha_value via pandas index
-        shap.force_plot(self.explainer.expected_value, self.shap_values[0, :], X, matplotlib=True)
+    def shap_summary_plot(self, X):
+        """
+        Plot features importance
+        """
+        shap.summary_plot(self.shap_values, X)
+        plt.savefig(os.path.join(self.mdl_config['mdl_path'], 'BDT_shap_values.png'))
+        plt.clf()
+
+        shap.summary_plot(self.shap_values, X, plot_type='bar')
+        plt.savefig(os.path.join(self.mdl_config['mdl_path'], 'BDT_shap_importance.png'))
+        plt.clf()
+
+    # ---------------------- PLOT ---------------------------
+    def loss_plot(self):
+        """
+        Plot loss for each boosting round.
+        Usefull if eval_set argument passed in .fit phase
+        """
+
+        train_curve = pd.DataFrame(self.mdl.evals_result()['validation_0'])
+        test_curve =  pd.DataFrame(self.mdl.evals_result()['validation_1'])
+
+        # Plot for each metric
+        for m in train_curve.columns:
+            fig, ax = plt.subplots()
+            ax.plot(train_curve.index, train_curve[m], label = 'Train')
+            ax.plot(test_curve.index, test_curve[m], label='Validation')
+            ax.legend()
+            plt.ylabel('{}'.format(m.upper()))
+            plt.xlabel('Boosting rounds')
+            plt.title('{} curve'.format(m.upper()))
+
+            plot_path = os.path.join(self.mdl_config['mdl_path'], 'BDT_{}_curve.png'.format(m.upper()))
+            plt.savefig(plot_path)
+
+            plt.clf()
+
+
 
 if __name__ == '__main__':
     from sklearn.datasets import load_boston
     from sklearn.model_selection import train_test_split
 
     mdl_config = {
+        'mdl_path': 'C:\\Users\\pc\\workspace\\clark_ml\\BDT_mdl_path',
         'params':{
             'n_estimators': 100,
             'max_depth':5
@@ -93,15 +153,23 @@ if __name__ == '__main__':
     }
 
     # Train/pred
-    X, y = load_boston(return_X_y=True)
+    data = load_boston(return_X_y=False)
+    X = pd.DataFrame(data['data'], columns=data['feature_names'])
+    y = pd.DataFrame(data['target'], columns = ['target'])
+
     X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=.8)
     mdl = clark_BDT(mdl_config = mdl_config)
-    mdl.fit(X_train, y_train)
-    y_pred = mdl.predict(X_test)
+    mdl.fit(X_train, y_train, eval_set = [(X_train, y_train), (X_test, y_test)], eval_metric = ['mae', 'rmse'])
+
+    # Plot loss
+    mdl.loss_plot()
 
     # Gridsearch
     # best_params = mdl.grid_search(X_train, y_train)
 
     # Shap
-    mdl.shap(X_test, y_test)
-    mdl.force_plot(X_test)
+    mdl._initialize_shap(X_train, y_train)
+    mdl.shap_summary_plot(X_train)
+
+    # Save mdl
+    mdl.save()
